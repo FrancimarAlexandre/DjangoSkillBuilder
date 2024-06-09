b2c89ce2152694be20db158a06f1aae90e24230b
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-06-09 20:39:18 -0300

    configurando arquivos estáticos (css,js,img)

--- Python_Django/Progressao/paginas/urls.py
+++ Python_Django/Progressao/paginas/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import IndexView
+from .views import *
 
 urlpatterns = [
-    path('inicio/', IndexView.as_view(),name = "inicio"),
+    path('', PaginaInicial.as_view(),name = "index"),
 ]
--- Python_Django/Progressao/paginas/views.py
+++ Python_Django/Progressao/paginas/views.py
@@ -2,5 +2,5 @@ from django.views.generic import TemplateView
 
 # Create your views here.
 
-class IndexView(TemplateView):
-    template_name = "index.html"
+class PaginaInicial(TemplateView):
+    template_name = "paginas/index.html"
