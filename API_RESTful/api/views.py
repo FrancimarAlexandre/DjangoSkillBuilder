2fa5f2b344f06f22e23f2dfe372e92ccefb9bc47
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-06-28 10:19:50 -0300

    autenticação

--- API_RESTful/api/views.py
+++ API_RESTful/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets,permissions
+from .serializers import UserSerializer,GroupSerializer
+from django.contrib.auth.models import Group,User
 # Create your views here.
+# ViewSets define o comportamento da view
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint da API que permite que os usuários sejam visualizados ou editados.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_class = [permissions.IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    Endpoint da API que permite que grupos sejam visualizados ou editados.
+    """
+    queryset = Group.objects.all().order_by('name')
+    serializer_class = GroupSerializer
+    permission_class = [permissions.IsAuthenticated]
--- API_RESTful/project_user/urls.py
+++ API_RESTful/project_user/urls.py
@@ -2,24 +2,18 @@
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth.models import User
-from rest_framework import routers,serializers,viewsets  
+from rest_framework import routers
+from api import views 
 
 
-# Serializer define a  representação da api
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url','username','email','is_staff']
 
-# ViewSets define o comportamento da view
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+
 
 # Routers fornecem uma maneira fácil de determinar  automaticamente a configuração do URL
 
 router = routers.DefaultRouter()
-router.register(r'users',UserViewSet) 
+router.register(r'users',views.UserViewSet) 
+router.register(r'groups',views.GroupViewSet) 
 urlpatterns = [
     path('',include(router.urls)),
     path('admin/', admin.site.urls),
